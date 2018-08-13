baa115228a7a690548dfcba30a6fee95b62b02f3
PoshTamer <galicea96@outlook.com>
2018-08-13 00:32:08 -0400

    modifying publish step

--- ci/Build.ps1
+++ ci/Build.ps1
@@ -92,6 +92,8 @@ if ($Ci) {
 
     if ($Publish) {
         Write-Verbose "Publishing VstsRestApiClient to the PSGallery..."
+        $Manifest = (Get-Content (Join-Path (Split-Path $PSScriptRoot -Parent) "VstsRestApiClient\VstsRestApiClient.psd1") -Raw)
+        $Manifest.Replace("[[COMMIT_HASH]]", $Env:APPVEYOR_REPO_COMMIT)
         Publish-Module -Path (Join-Path (Split-Path $PSScriptRoot -Parent) "VstsRestApiClient") -NuGetApiKey $Env:PSGALLERY_TOKEN
     }
 }
